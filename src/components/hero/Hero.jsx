aaafbb4af4e20033904d3d2507141625fb12c637
Asliddin-Coder <electusprogrammer@gmail.com>
2025-09-22 13:00:20 +0500

    hero desktop version completed

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,32 +5,38 @@ import person from "../../assets/images/person.png";
 const Hero = () => {
   return (
     <>
-        <div className={styles.hero}> 
-          <div className={styles.container}>
-            {/* Hello start */}
-            <div className={styles.hello}>
-              <span><span id={styles.first}>Hello,</span> I'm</span>
-              <h1>VICTOR</h1>
-              <span className={styles.job}>Professional English Teacher</span>
-              <button className={styles.letsTalkBtn}>Let's Talk</button>
-            </div>
-            {/* Hello end */}
-            {/* TEXT */}
-            <h1 className={styles.placeholder}>VICTOR</h1>
-            {/* Images */}
-            <img id={styles.person} src={person} alt="" />
-            <img id={styles.backImg} src={backImg} alt="" />
-            {/* Social Media start */}
-            <div className={styles.socialMedia}>
-                <FaInstagram size={40} className={styles.btn} />
-                <FaFacebookF size={40} className={styles.btn} />
-                <FaLinkedinIn size={40} className={styles.btn} />
-                <FaTelegram size={40} className={styles.btn} />
-            </div>
-            {/* Social Media end */}
-            <div className={styles.bottomBox}></div>
+      <div className={styles.hero}>
+        <div className={styles.container}>
+          {/* Hello start */}
+          <div className={styles.hello}>
+            <span><span id={styles.first}>Hello,</span> I'm</span>
+            <h1>VICTOR</h1>
+            <span className={styles.job}>Professional English Teacher</span>
+            <button className={styles.letsTalkBtn}>Let's Talk</button>
           </div>
+          {/* Hello end */}
+          {/* TEXT */}
+          <h1 className={styles.placeholder}>VICTOR</h1>
+          {/* Lines */}
+          <div className={styles.lines}>
+            <div className={styles.line}>A</div>
+            <div className={styles.line}>BB</div>
+            <div className={styles.line}>C</div>
+          </div>
+          {/* Images */}
+          <img id={styles.person} src={person} alt="" />
+          <img id={styles.backImg} src={backImg} alt="" />
+          {/* Social Media start */}
+          <div className={styles.socialMedia}>
+            <FaInstagram size={40} className={styles.btn} />
+            <FaFacebookF size={40} className={styles.btn} />
+            <FaLinkedinIn size={40} className={styles.btn} />
+            <FaTelegram size={40} className={styles.btn} />
+          </div>
+          {/* Social Media end */}
+          <div className={styles.bottomBox}></div>
         </div>
+      </div>
     </>
   )
 }
